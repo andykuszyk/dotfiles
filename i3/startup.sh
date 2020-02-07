4e50d2ef2915a30ec1153731279e563648f60969
Andy Kuszyk <andy.kuszyk@form3.tech>
2020-02-07 10:28:58 +0000

    Added gnome keyring and background

--- i3/startup.sh
+++ i3/startup.sh
@@ -6,3 +6,9 @@ xmodmap -e "keycode 92 = Super_R"
 
 # Screen resolution and layout
 xrandr --output DP-3 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-1 --mode 1280x1024 --pos 0x232 --rotate normal --output eDP-1 --mode 1920x1080 --pos 3200x312 --rotate normal --output DP-2 --off
+
+# Gnome keyring
+gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
+
+# Wallpaper
+feh --randomize --bg-fill ~/Pictures/wallpapers/
