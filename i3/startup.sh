a198ee7498c8789a9a828fbc2ba868100647437c
Andy Kuszyk <andy.kuszyk@form3.tech>
2020-02-17 18:09:55 +0000

    Added compton config

--- i3/startup.sh
+++ i3/startup.sh
@@ -14,4 +14,4 @@ gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
 feh --randomize --bg-fill ~/Pictures/wallpapers/
 
 # Start compton
-compton --daemon
+compton --daemon --config ~/dotfiles/compton.conf
