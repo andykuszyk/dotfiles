94a2273732f9b55db70ef14669d42913fe3185a8
Andy Kuszyk <andy.kuszyk@form3.tech>
2020-02-17 18:09:27 +0000

    Removed screen temperature

--- i3/startup.sh
+++ i3/startup.sh
@@ -7,9 +7,6 @@ xmodmap -e "keycode 92 = Super_R"
 # Screen resolution and layout
 xrandr --output DP-3 --primary --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-1 --mode 1280x1024 --pos 0x232 --rotate normal --output eDP-1 --mode 1920x1080 --pos 3200x312 --rotate normal --output DP-2 --off
 
-# Set screen temperature
-redshift -O 4500
-
 # Gnome keyring
 gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
 
