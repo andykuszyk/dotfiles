44e91a08de31e991c505ba8296a642462371abbe
Andy Kuszyk <andy.kuszyk@form3.tech>
2021-01-13 13:15:27 +0000

    Prevented lock screen from hanging on git operations

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -3,6 +3,6 @@ tmpbg='/tmp/screen.png'
 scrot "$tmpbg"
 convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
 cd ~/repos/andykuszyk/captains-log
-./lg stop
+./lg stop &
 i3lock -n -i "$tmpbg"
 ./lg start
