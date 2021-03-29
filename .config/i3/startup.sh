5470ebd85879c4d8987b8d225838ebb9dd75ef3d
andykuszyk <andy.kuszyk@form3.tech>
2021-03-29 08:50:21 +0100

    Updated screen layout

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -5,7 +5,7 @@ xmodmap -e "keycode 108 = Super_R"
 xmodmap -e "keycode 92 = Super_R"
 
 # Screen resolution and layout
-xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-6 --off --output DP-7 --mode 1920x1080 --pos 0x125 --rotate normal
+xrandr --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 3200x0 --rotate left --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --mode 1920x1080 --pos 1280x0 --rotate normal --output DP-6 --off --output DP-7 --mode 1280x1024 --pos 0x56 --rotate normal
 
 # Gnome keyring
 gnome-keyring-daemon --start --components=pkcs11,secrets,ssh,gpg
