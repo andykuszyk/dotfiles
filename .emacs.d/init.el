a547925f67e1f6a4327d53d5ecf924d790720481
andykuszyk <pairofsocks@hotmail.co.uk>
2023-12-01 11:30:21 +0000

    Added macos-specific config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,3 @@
-(toggle-frame-maximized)
+(when (string= system-type "darwin")
+  (org-babel-load-file (concat user-emacs-directory "darwin.org")))
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
