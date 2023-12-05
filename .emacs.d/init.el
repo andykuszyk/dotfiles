fadec2229600da8bd0d2b88942f196c181f94f82
andykuszyk <pairofsocks@hotmail.co.uk>
2023-12-05 11:44:02 +0000

    Moved linux-specific configuration into its own file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,5 @@
 (when (string= system-type "darwin")
   (org-babel-load-file (concat user-emacs-directory "darwin.org")))
+(when (string= system-type "gnu/linux")
+  (org-babel-load-file (concat user-emacs-directory "linux.org")))
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
