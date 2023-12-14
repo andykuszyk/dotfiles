5b8cf809dad7b7afd87a0f8f11b536b7e6dd6f43
andykuszyk <pairofsocks@hotmail.co.uk>
2023-12-14 14:55:16 +0000

    Got listing issues in a new buffer to work

--- .emacs.d/jira.el
+++ .emacs.d/jira.el
@@ -1,7 +1,14 @@
 (defun jira-list-my-issues ()
   "Lists the issues assigned to you"
   (interactive)
-  (shell-command "jira issues list -a $(jira me) -s~Done --plain"))
+  (let ((buffer (get-buffer-create "jira list")))
+    (with-current-buffer buffer
+      (read-only-mode -1)
+      (kill-region (point-min) (point-max)))
+    (call-process "sh" nil buffer nil "-c" "jira issues list -a $(jira me) -s~Done --plain")
+    (with-current-buffer buffer
+      (read-only-mode nil))
+    (display-buffer buffer)))
 
 (defun jira-view-issue (reference)
   "Prints the description of an issue"
