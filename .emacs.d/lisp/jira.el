3e7515ea9b077bfd2d7eaaac0baa1095af56a2fe
andykuszyk <pairofsocks@hotmail.co.uk>
2024-01-02 13:49:07 +0000

    Some cleanup, and the ability to toggle done issues

--- .emacs.d/lisp/jira.el
+++ .emacs.d/lisp/jira.el
@@ -1,4 +1,4 @@
-;;;  jira.el --- a simple wrapper around the JIRA CLI
+;;;  jira.el --- a simple wrapper around the JIRA CLI -*- lexical-binding: t -*-
 
 ;;; Commentary:
 ;;; This file contains some simple functions that wrap around the JIRA CLI.
@@ -26,104 +26,150 @@
 ;;;
 ;;;    (setq jira-host "https://my-org.atlassian.net")
 
-;;; Code
+;;; Code:
 
-(setq jira-host "")
+(require 'ansi-color)
 
-(setq display-buffer-alist '(("jira" display-buffer-same-window)))
+(defvar jira--last-reported-flag)
 
-(setq jira--current-issue-reference nil)
+(defvar jira--last-exclude-done t)
 
-(setq jira--current-screen "list")
+(defvar jira-host
+  nil
+  "The URL of your JIRA instance, e.g. https://my-org.atlassian.net.")
+
+(defvar jira--current-issue-reference
+  nil
+  "The issue reference of the currently open issue.")
+
+(defvar jira--current-screen
+  "list"
+  "The name of the screen-type which is currently being displayed.")
 
 (defun jira--parse-issue-reference (text)
-  "Tries to parse the JIRA issue reference from the provided text"
+  "Try to parse the JIRA issue reference from the provided TEXT."
   (if (string-match "[A-Z]+\\-[0-9]+" text)
       (match-string 0 text)
     nil))
 
 (defun jira--get-current-issue-reference ()
-  "Gets the current line's jira issue reference"
+  "Gets the current line's jira issue reference."
   (if (string= jira--current-screen "list")
-      (jira--parse-issue-reference (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+      (jira--parse-issue-reference
+       (buffer-substring-no-properties
+	(line-beginning-position)
+	(line-end-position)))
     jira--current-issue-reference))
 
 (defun jira--kill-ring-save-reference ()
+  "Save the reference of the current issue to the kill ring."
   (interactive)
   (let ((reference (jira--get-current-issue-reference)))
     (message (format "killed reference: %s" reference))
     (kill-new reference)))
 
 (defun jira--view-current-issue ()
-  "Views the issue represented by the current line"
+  "Views the issue represented by the current line."
   (interactive)
   (jira-view-issue (jira--get-current-issue-reference)))
 
 (defun jira--open-in-browser (reference)
-  (message (format "opening issue in browser: %s" (format "%s/browse/%s" jira-host reference)))
-  (browse-url (format "%s/browse/%s" jira-host reference)))
+  "Opens the current issue REFERENCE in an external web browser."
+  (let ((url (format "%s/browse/%s" jira-host reference)))
+    (message (format "opening issue in browser: %s" url))
+    (browse-url (format "%s/browse/%s" jira-host url))))
 
 (defun jira--view-in-browser ()
-  "Opens the current issue in the browser"
+  "Opens the current issue in the browser."
   (interactive)
   (jira--open-in-browser (jira--get-current-issue-reference)))
 
 (defun jira--set-keys ()
-  "Sets key bindings for JIRA buffers"
+  "Set key bindings for JIRA buffers."
   (local-set-key (kbd "q") #'quit-window)
   (local-set-key (kbd "n") #'next-line)
   (local-set-key (kbd "p") #'previous-line)
   (local-set-key (kbd "l") #'jira-list-my-assigned-issues)
   (local-set-key (kbd "a") #'jira-list-my-assigned-issues)
   (local-set-key (kbd "r") #'jira-list-my-reported-issues)
+  (local-set-key (kbd "x") #'jira-toggle-exclude-done-issues)
   (local-set-key (kbd "o") #'jira--view-in-browser)
   (local-set-key (kbd "w") #'jira--kill-ring-save-reference)
   (local-set-key (kbd "RET") #'jira--view-current-issue))
 
 (defun jira ()
-  "Lists issues assigned to you"
+  "Lists issues assigned to you."
   (interactive)
   (jira-list-my-assigned-issues))
 
 (defun jira-list-my-assigned-issues ()
-  "Lists issues assigned to you"
+  "Lists issues assigned to you."
   (interactive)
-  (jira--list-my-issues "-a"))
+  (jira--list-my-issues "-a" jira--last-exclude-done))
 
 (defun jira-list-my-reported-issues ()
-  "Lists issues reported by you"
+  "Lists issues reported by you."
   (interactive)
-  (jira--list-my-issues "-r"))
+  (jira--list-my-issues "-r" jira--last-exclude-done))
 
-(defun jira--list-my-issues (flag)
-  "Lists the issues assigned or reported to you"
+(defun jira-toggle-exclude-done-issues ()
+  "Toggle whether or not to display done issues."
+  (interactive)
+  (jira--list-my-issues
+   jira--last-reported-flag
+   (not  jira--last-exclude-done)))
+
+(defun jira--list-my-issues (reported-flag exclude-done)
+  "Lists the issues assigned or reported to you, according to REPORTED-FLAG \\
+and EXCLUDE-DONE."
+  (setq jira--last-reported-flag reported-flag)
+  (setq jira--last-exclude-done exclude-done)
   (setq jira--current-screen "list")
-  (let ((buffer (get-buffer-create "jira")))
+  (let ((buffer (get-buffer-create "jira"))
+	(inhibit-read-only t))
     (with-current-buffer buffer
-      (read-only-mode -1)
       (erase-buffer)
-      (call-process "sh" nil buffer nil "-c" (format "jira issues list %s $(jira me) -s~Done --plain --columns 'TYPE,KEY,SUMMARY,STATUS,ASSIGNEE,REPORTER'" flag))
+      (call-process
+       "sh"
+       nil
+       buffer
+       nil
+       "-c"
+       (format (concat
+		"jira issues list"
+		" %s $(jira me)"
+		(if exclude-done " -s~Done" "")
+		" --plain"
+		" --columns 'TYPE,KEY,SUMMARY,STATUS,ASSIGNEE,REPORTER'")
+	       reported-flag))
       (jira--set-keys)
       (read-only-mode t)
       (goto-char (point-min)))
     (display-buffer buffer)))
 
 (defun jira-view-issue (reference)
-  "Prints the description of an issue"
+  "Prints the description of the issue REFERENCE."
   (interactive "sEnter the issue number: ")
   (setq jira--current-screen "view")
   (setq jira--current-issue-reference reference)
   (if (string= reference "")
       (message "No reference provided")
-    (let ((buffer (get-buffer-create "jira")))
+    (let ((buffer (get-buffer-create "jira"))
+	  (inhibit-read-only t))
       (with-current-buffer buffer
-	(read-only-mode -1)
 	(erase-buffer)
-	(call-process "sh" nil buffer nil "-c" (format "jira issue view %s | cat" reference))
+	(call-process
+	 "sh"
+	 nil
+	 buffer
+	 nil
+	 "-c"
+	 (format "jira issue view %s | cat" reference))
 	(jira--set-keys)
 	(ansi-color-apply-on-region (point-min) (point-max))
 	(read-only-mode t)
-	(goto-char (point-min))))))
+	(goto-char (point-min))
+	(display-buffer buffer)))))
 
 (provide 'jira)
 ;;; jira.el ends here
