09ff2548d2031a0cb56e4fb6a8d6fbb4b7ea9a47
andykuszyk <pairofsocks@hotmail.co.uk>
2024-09-06 10:35:21 +0100

    Commented init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,23 +1,57 @@
+;; I run a single Emacs instance in server mode, and use emacsclient to connect
+;; to it if I want an ephemeral editor.
 (server-start)
 
+;; Use MELPA as a package repository, and load use-package for managing package
+;; configuration.
 (org-babel-load-file (concat user-emacs-directory "package-management.org"))
+
+;; Configure tools and keybindings for managing Emacs windows.
 (org-babel-load-file (concat user-emacs-directory "window-management.org"))
+
+;; Variouus UI customisations, as well as some fundamental usability improvements.
 (org-babel-load-file (concat user-emacs-directory "ui.org"))
+
+;; Configure how Emacs stores backup files.
 (org-babel-load-file (concat user-emacs-directory "backups.org"))
+
+;; Configure Emacs' environment.
 (org-babel-load-file (concat user-emacs-directory "environment.org"))
+
+;; Code project management tools.
 (org-babel-load-file (concat user-emacs-directory "projects.org"))
+
+;; Narrowing and completion tools for the minibuffer, and buffers.
 (org-babel-load-file (concat user-emacs-directory "completion.org"))
+
+;; Configure Emacs as a terminal emulator.
 (org-babel-load-file (concat user-emacs-directory "terminal.org"))
+
+;; Configure various tools for viewing documentaiton in Emacs.
 (org-babel-load-file (concat user-emacs-directory "documentation.org"))
+
+;; Various custom functions and keybindings.
 (org-babel-load-file (concat user-emacs-directory "ak.org"))
+
+;; Specific configuration for Mac OS, and Linux.
 (if (string= system-type "darwin")
     (org-babel-load-file (concat user-emacs-directory "darwin.org"))
   (org-babel-load-file (concat user-emacs-directory "linux.org")))
+
+;; General and miscellaneous Emacs configuration.
 (org-babel-load-file (concat user-emacs-directory "emacs.org"))
+
+;; Org-mode configuration.
 (org-babel-load-file (concat user-emacs-directory "org.org"))
+
+;; Configuration for a variety of different programming languages.
 (org-babel-load-file (concat user-emacs-directory "languages.org"))
+
+;; Load custom elisp packages that I haven't broken out into separate repos yet.
 (add-to-list 'load-path "~/.emacs.d/lisp")
 (require 'jira)
 (require 'github)
+
+;; Load any machine-specific initialisation, if it exists.
 (if (file-exists-p "~/.emacs.d/lisp/custom-init.el")
     (load-file "~/.emacs.d/lisp/custom-init.el"))
