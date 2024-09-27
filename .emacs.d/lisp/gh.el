bb635c66303ce1e7699f70a7e71c06dd38567680
andykuszyk <git@akuszyk.com>
2024-09-27 13:11:16 +0100

    Fixed json powered search command

--- .emacs.d/lisp/gh.el
+++ .emacs.d/lisp/gh.el
@@ -33,7 +33,7 @@ The value of this variable is case-sensitive, and must match the case used in
 (defun gh-kill-name ()
   (interactive)
   (let ((line (buffer-substring-no-properties (pos-bol) (pos-eol))))
-    (unless (string-match (format "^\\(%s/[A-za-z0-9-_]+\\).*" gh-owner) line)
+    (unless (string-match (format "^.*\\(%s/[A-za-z0-9-_]+\\).*" gh-owner) line)
       (error "Couldn't find a repo name on the current line"))
     (let ((match (match-string 1 line)))
       (message (format "Killed repo name: %s" match))
@@ -64,5 +64,59 @@ The value of this variable is case-sensitive, and must match the case used in
 	(dired repo-path))))
 
 (global-set-key (kbd "C-x A h s") #'gh-search-repos)
+(global-set-key (kbd "C-x A h j") #'gh-search-repos-json)
+
+(defun gh-search-repos-json (name)
+  (interactive "sRepo name: ")
+  (let ((gh-buffer (get-buffer-create "*gh*"))
+	(json-output
+	 (json-parse-string
+	  (shell-command-to-string
+	   (format
+	    "gh search repos --json 'name,description,visibility,isArchived,fullName' --owner %s %s"
+	    gh-owner
+	    name)))))
+    (with-current-buffer gh-buffer
+      (tabulated-list-mode)
+      (setq tabulated-list-format
+	    (vector
+	     '("Cloned?" 10 t . nil)
+	     '("Name" 40 t . nil)
+	     '("Description" 80 t . nil)
+	     '("Status" 30 t . nil)
+	     ))
+      (setq tabulated-list-entries nil)
+      (dolist (repo (append json-output nil))
+	(let* ((full-name (gethash "fullName" repo))
+	       (cloned (gh--get-clone-status (gethash "name" repo)))
+	       (description (gethash "description" repo))
+	       (status (gh--get-repo-status
+			(gethash "visibility" repo)
+			(gethash "isArchived" repo))))
+	  (add-to-list
+	   'tabulated-list-entries
+	   (list nil (vector cloned full-name description status)))))
+      (tabulated-list-init-header)
+      (tabulated-list-print)
+      (local-set-key "w" #'gh-kill-name)
+      (local-set-key "o" #'gh-open-in-browser)
+      (local-set-key "C" #'gh-clone-repo)
+      (local-set-key "d" #'gh-dired-repo)
+      )
+    (display-buffer gh-buffer)) )
+
+(defun gh--get-clone-status (name)
+  (let ((repo-path (format "%s/%s" gh-clone-path name)))
+    (if (file-directory-p repo-path)
+	"Y"
+      "N")))
+
+(defun gh--get-repo-status (visibility is-archived)
+  (format
+   "%s%s"
+   visibility
+   (if (string= is-archived "t")
+       ",archived"
+     "")))
 
 (provide 'gh)
