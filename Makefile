2d47684eb381df772b83be44d6e6f6aab59d2394
andykuszyk <git@akuszyk.com>
2025-01-27 16:22:02 +0000

    Fixed default target

--- Makefile
+++ Makefile
@@ -1,17 +1,4 @@
-dirs:
-	mkdir -p ~/.config/i3
-	mkdir -p ~/.config/gtk-3.0
-	mkdir -p ~/.config/powerline/themes/wm
-	mkdir -p ~/.config/powerline/themes/tmux
-	mkdir -p ~/.config/powerline/themes/shell
-	mkdir -p ~/.vim
-	mkdir -p ~/repos
-	mkdir -p ~/.local/bin
-	mkdir -p ~/.vim/swapfiles
-	mkdir -p ~/.ctags.d
-	mkdir -p ~/.termux/
-
-default: dirs
+default:
 	bash install.sh
 
 install-packages:
--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir -p ~/.emacs.d
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/gtk-3.0
+mkdir -p ~/.config/powerline/themes/wm
+mkdir -p ~/.config/powerline/themes/tmux
+mkdir -p ~/.config/powerline/themes/shell
+mkdir -p ~/.vim
+mkdir -p ~/repos
+mkdir -p ~/.local/bin
+mkdir -p ~/.vim/swapfiles
+mkdir -p ~/.ctags.d
+mkdir -p ~/.termux/
+
 for f in $(find . -type f | grep -v git); do
     cp $f $HOME/$(echo $f | sed 's/\.\///g');
 done
